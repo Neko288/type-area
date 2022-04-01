d1ec050e067caf5326df6d2a24dfa36da560aff7
Neko288 <ryzen.kuro@outlook.jp>
2022-04-01 09:46:20 +0900

    add dark mode(white mode)

--- src.js
+++ src.js
@@ -1,12 +1,11 @@
-var urlHash = location.hash;
-var body = document.getElementById('#body');
-var text_area = document.getElementById('#text-area');
-if(urlHash){
-  if (urlHash == '#dark') {
-    body.style.background = '#171411';
-    body.style.color = '#e90718';
-    text_area.style.background = '#171411';
-    text_area.style.color = '#e90718';
-  }
+function white(){
+  let text_area = document.getElementById('text-area');
+  document.body.style.background = '#fffdfe';
+  document.body.style.color = '#9a0510';
+  text_area.style.background = '#fffdfe';
+  text_area.style.color = '#9a0510';
 }
 document.getElementById('text-area').focus();
+function reload(){
+  window.location.reload();
+}
